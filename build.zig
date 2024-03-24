8abbc62f13a1fcd041877aa7ffdd7d7932c6c03c
terrablue <102580937+terrablue@users.noreply.github.com>
2024-03-24 01:44:02 +0100

    add fpconv dep

--- build.zig
+++ build.zig
@@ -10,6 +10,9 @@ pub fn build(b: *Builder) void {
     const hirediz = b.dependency("hirediz", .{
         .target = target,
     });
+    const fpconv = b.dependency("fpconv", .{
+        .target = target,
+    });
 
     const rediz = b.addExecutable(.{
         .name = "rediz",
@@ -18,16 +21,29 @@ pub fn build(b: *Builder) void {
     });
     rediz.installLibraryHeaders(hdr.artifact("hdr_histogram"));
     rediz.installLibraryHeaders(hirediz.artifact("hirediz"));
+    rediz.installLibraryHeaders(fpconv.artifact("fpconv"));
     rediz.linkLibrary(hdr.artifact("hdr_histogram"));
     rediz.linkLibrary(hirediz.artifact("hirediz"));
+    rediz.linkLibrary(fpconv.artifact("fpconv"));
 
     b.installArtifact(rediz);
     rediz.linkLibC();
     rediz.addCSourceFiles(&.{
+        "src/acl.c",
         "src/ae.c",
         "src/aof.c",
+        "src/cluster.c",
+        "src/cluster_legacy.c",
+        "src/config.c",
+        "src/db.c",
+        "src/defrag.c",
+        "src/evict.c",
+        "src/memtest.c",
+        "src/module.c",
         "src/sentinel.c",
         "src/server.c",
+        "src/syscheck.c",
+        "src/rdb.c",
         "src/threads_mngr.c",
     }, &.{
         "-std=c99",
